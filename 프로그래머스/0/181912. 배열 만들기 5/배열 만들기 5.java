05c8827f1af7ee9a05728cc4df3bd65c9a7419bd
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2025-07-02 18:08:53 +0900

    [level 0] Title: 배열 만들기 5, Time: 8.71 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] intStrs, int k, int s, int l) {
+        return Arrays.stream(intStrs)
+                     .mapToInt(str -> Integer.parseInt(str.substring(s, s+l)))
+                     .filter(val -> val > k).toArray();
+    }
+}
