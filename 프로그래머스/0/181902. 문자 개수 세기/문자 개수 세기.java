85c90560eaf686b1c1f4532283a5ec25e602104b
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2025-07-08 13:35:55 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.10 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -2,18 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        Map<Character, Integer> map = new HashMap<>();
-        
-        for(char i = 'A'; i <= 'z' ; i++){
-            if(i > 'Z' && i < 'a') continue;
-            map.put(i, 0);
-        }
+        int[] arr = new int[52];
         
         for(int i = 0; i < my_string.length(); i++){
-            Character c = my_string.charAt(i);
-            map.put(c, map.get(c) + 1);
+            int index = my_string.charAt(i) >= 'a' ? my_string.charAt(i) -'a' + 26 : my_string.charAt(i) - 'A';
+            arr[index] = arr[index] + 1;
         }
         
-        return map.values().stream().mapToInt(Integer::valueOf).toArray();
+        return arr;
     }
 }
