bedcb8f1cd9e3093e9549d585d197d06ec1190e9
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2025-07-09 23:41:26 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/README.md
+++ 프로그래머스/0/181899. 카운트 다운/README.md
@@ -1,10 +1,10 @@
 # [level 0] 카운트 다운 - 181899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899?language=java) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 7.90 ms
+메모리: 82.3 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 09일 23:34:58
+2025년 07월 09일 23:41:24
 
 ### 문제 설명
 
--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,7 +1,11 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        return IntStream.rangeClosed(end_num, start_num).map(i -> start_num + end_num - i).toArray(); 
+        int[] arr = new int[start_num - end_num + 1];
+
+        for(int i = 0; i < start_num - end_num + 1; i++){
+            arr[i] = start_num - i;
+        }
+
+        return arr;
     }
 }
