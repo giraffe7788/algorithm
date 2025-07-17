2b7763ec33e54ac73927acce6aa6ac505b9eef06
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2025-07-17 21:46:01 +0900

    [level 0] Title: 가까운 1 찾기, Time: 4.29 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] arr, int idx) {
+        return IntStream.range(0, arr.length)
+                        .filter(i -> i >= idx && arr[i] == 1)
+                        .findFirst()
+                        .orElse(-1);
+    }
+}
