6d41a03e09816f11e02d889b9263a9d6f99b502a
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2025-11-20 13:43:53 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 13.64 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        for(int i = 0; i < myString.length(); i++) {
+            char ch = myString.charAt(i);
+            if(ch <= 'Z') {
+                ch += 32;
+            }
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
