e5527033ee273be212f1c77c3bf2c117ada06277
giraffe7788 <139102439+giraffe7788@users.noreply.github.com>
2026-04-07 00:09:07 +0900

    [level 0] Title: 세 개의 구분자, Time: 48.86 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/README.md
+++ 프로그래머스/0/181862. 세 개의 구분자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 세 개의 구분자 - 181862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181862) 
 
 ### 성능 요약
 
-메모리: 107 MB, 시간: 35.43 ms
+메모리: 116 MB, 시간: 48.86 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 06일 23:58:36
+2026년 04월 07일 00:09:04
 
 ### 문제 설명
 
--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -1,13 +1,10 @@
 import java.util.Arrays;
 
 class Solution {
-    public String[] solution(String myStr) {
-        myStr = myStr.replace("a", "1").replace("b", "1").replace("c", "1");
-
-        String[] result = Arrays.stream(myStr.split("1"))
-            .filter(s -> !s.trim().isEmpty())
-            .toArray(String[]::new);
-
+    public String[] solution (String myStr){
+        String[] result = Arrays.stream(myStr.split("[abc]+"))
+                .filter(s -> !s.isEmpty())
+                .toArray(String[]::new);
         return result.length == 0 ? new String[]{"EMPTY"} : result;
     }
 }
